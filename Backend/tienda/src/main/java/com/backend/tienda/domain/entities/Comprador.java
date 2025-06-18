67a3c7532f53b89f0ad35e1076ee9d98fc224ab1
blicsi <pablorubengross@gmail.com>
2025-06-18 20:34:12 -0300

    comprador mitad hecho

--- Backend/tienda/src/main/java/com/backend/tienda/domain/entities/Comprador.java
+++ Backend/tienda/src/main/java/com/backend/tienda/domain/entities/Comprador.java
@@ -1,4 +1,18 @@
 package com.backend.tienda.domain.entities;
 
-public class Comprador {
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@DiscriminatorValue(value = "Comprador")
+public class Comprador extends Usuario{
+    private String Cart_id;
+
+    public Comprador(String ID, String Username, String Password) {
+        super(ID, Username, Password);
+    }
 }
---
+++ Backend/tienda/src/main/java/com/backend/tienda/domain/entities/Producto.java
@@ -0,0 +1,4 @@
+package com.backend.tienda.domain.entities;
+
+public class Producto {
+}
