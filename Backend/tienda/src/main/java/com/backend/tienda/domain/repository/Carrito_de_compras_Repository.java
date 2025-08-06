6c5fc773fa9d2eee0bf6779f1a9adccf1939b3a2
blicsi <pablorubengross@gmail.com>
2025-08-05 22:59:42 -0300

    little fix en repositorio

--- Backend/tienda/src/main/java/com/backend/tienda/domain/repository/Carrito_de_compras_Repository.java
+++ Backend/tienda/src/main/java/com/backend/tienda/domain/repository/Carrito_de_compras_Repository.java
@@ -4,9 +4,12 @@ import com.backend.tienda.domain.entities.Carrito_de_compras;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface Carrito_de_compras_Repository extends JpaRepository <Carrito_de_compras, Long> {
 
-
+    Optional<Carrito_de_compras> findByCompradorId(Long compradorId);
 
 }
